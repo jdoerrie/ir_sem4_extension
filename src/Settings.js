b5e319cd177b3c4ab1e40d3c6e47a41a29009206
Jan Wilken Dörrie <dojan@ethz.ch>
2015-06-29 11:53:01 +0200

    Made Endpoint URL part of Settings

--- src/Settings.js
+++ src/Settings.js
@@ -1,2 +1,5 @@
 var Settings = {};
+
 Settings.DisableJSonGooglePages = false;
+// Settings.Endpoint = "http://localhost:8000/polls/1/vote/";
+Settings.Endpoint = "http://localhost:8000/queries/post/";
