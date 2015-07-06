2ab5f5af5825c56da392cee87f38ef4c363946cd
Jan Wilken Dörrie <dojan@ethz.ch>
2015-07-06 17:14:48 +0200

    Use Server Endpoints

--- src/Settings.js
+++ src/Settings.js
@@ -1,5 +1,8 @@
-var Settings = {};
-
-Settings.DisableJSonGooglePages = false;
-// Settings.Endpoint = "http://localhost:8000/polls/1/vote/";
-Settings.Endpoint = "http://localhost:8000/queries/post/";
+/*exported Settings */
+var Settings = {
+  DisableJSonGooglePages: false,
+  // Endpoint:  'http://localhost:8000/queries/post/',
+  // Selectors: 'http://localhost:8000/queries/get/selectors/'
+  Endpoint:  'http://idvm-infk-hofmann01.inf.ethz.ch/queries/post/',
+  Selectors: 'http://idvm-infk-hofmann01.inf.ethz.ch/queries/get/selectors/'
+};
