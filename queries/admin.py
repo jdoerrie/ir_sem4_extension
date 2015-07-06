54761ecb1694b95ea1dc764ac6a4fd46b20aca13
Jan Wilken Dörrie <dojan@ethz.ch>
2015-07-06 17:13:42 +0200

    Improve Admin Interface

--- queries/admin.py
+++ queries/admin.py
@@ -1,9 +1,68 @@
 from django.contrib import admin
 
 from .models import User, Hover, SearchQuery, SearchResult, Click
+
+length = 50
+
+
+class UserAdmin(admin.ModelAdmin):
+    fields = list_display = ('user_id', 'user_agent')
+
+
+class HoverAdmin(admin.ModelAdmin):
+    list_display = ('text', 'short_url', 'page_x', 'page_y', 'duration',
+                    'get_user', 'timestamp')
+
+    def short_url(self, obj):
+        return obj.url[:length] + ('', '...')[len(obj.url) > length]
+
+    def get_user(self, obj):
+        return obj.user.user_id
+
+    get_user.short_description = 'User'
+    get_user.admin_order_field = 'user__user_id'
+
+
+class SearchQueryAdmin(admin.ModelAdmin):
+    list_display = ('query', 'engine', 'get_user', 'timestamp')
+
+    def get_user(self, obj):
+        return obj.user.user_id
+
+    get_user.short_description = 'User'
+    get_user.admin_order_field = 'user__user_id'
+
+
+class SearchResultAdmin(admin.ModelAdmin):
+    list_display = ('text', 'rank', 'short_link', 'short_desc',
+                    'get_search_query')
+
+    def short_desc(self, obj):
+        return obj.desc[:length] + ('', '...')[len(obj.desc) > length]
+
+    def short_link(self, obj):
+        return obj.link[:length] + ('', '...')[len(obj.link) > length]
+
+    def get_search_query(self, obj):
+        return obj.search_query.query
+
+    get_search_query.short_description = 'Search Query'
+    get_search_query.admin_order_field = 'search_query__query'
+
+
+class ClickAdmin(admin.ModelAdmin):
+    list_display = ('short_link', 'short_url', 'page_x', 'page_y', 'text',
+                    'search_result', 'user', 'timestamp')
+
+    def short_url(self, obj):
+        return obj.url[:length] + ('', '...')[len(obj.url) > length]
+
+    def short_link(self, obj):
+        return obj.link[:length] + ('', '...')[len(obj.link) > length]
+
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Hover)
-admin.site.register(SearchQuery)
-admin.site.register(SearchResult)
-admin.site.register(Click)
+admin.site.register(User, UserAdmin)
+admin.site.register(Hover, HoverAdmin)
+admin.site.register(SearchQuery, SearchQueryAdmin)
+admin.site.register(SearchResult, SearchResultAdmin)
+admin.site.register(Click, ClickAdmin)
